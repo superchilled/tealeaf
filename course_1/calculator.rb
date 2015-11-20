1da88814e2188bfe4e0f3c362594b986e4a3ab6f
superchilled <karl@superchilled.co.uk>
2015-11-20 12:09:00 +0000

    slightly refactored calculator

--- course_1/calculator.rb
+++ course_1/calculator.rb
@@ -1,14 +1,39 @@
 # calculator.rb 
+# Refactor:
+# 1) do while loop to get number and make sure it's a number
+# 2) do while loop to get the operator
+# 3) extract get number loop to a method
 
 def say(msg)
   puts "=> #{msg}"
 end
 
-say "What's the first number?"
-num1 = gets.chomp
+def is_num(str)
+  if str =~ /\A[-+]?\d+\z/
+    return true
+  else
+    return false
+  end
+end
 
-say "What's the second number?"
-num2 = gets.chomp
+num1 = ""
+num2 = ""
+
+loop do
+  say "What's the first number?"
+  num1 = gets.chomp
+  if is_num(num1)
+    break
+  end
+end
+
+loop do
+  say "What's the second number?"
+  num2 = gets.chomp
+  if is_num(num2)
+    break
+  end
+end
 
 say "1) add 2) subtract 3) multiply 4) divide"
 operator = gets.chomp
@@ -24,6 +49,5 @@ else
 end
 
 puts "Result is #{result}"
-
 
 
